a0c1067094b18b0c9432854b6546069a3791c2c5
sachipatel <sachixl300@gmail.com>
2026-05-12 15:13:12 +0530

    fs

--- convert_model.py
+++ convert_model.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 
-# Load old model
+print("Loading old model...")
+
 model = tf.keras.models.load_model(
-    "plant_model.h5",
+    "old_model.h5",
     compile=False
 )
 
-# Save in new format
-model.save("new_plant_model.h5")
+print("Saving new model...")
 
-print("Model converted successfully!")
+model.save("plant_model.h5")
+
+print("DONE")
